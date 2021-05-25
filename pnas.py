77a458748a50a169daad4a63c9eeb4d95afe8ff3
nihanyatir <nihanyatir@gmail.com>
2021-05-26 00:57:47 +0300

    created a list of links for pdf documents

--- pnas.py
+++ pnas.py
@@ -1,25 +1,27 @@
 import requests
 from bs4 import BeautifulSoup
 
-#https://www.pnas.org/content/by/section/Psychological%20and%20Cognitive%20Sciences?page=1
-#sayfa sayisi 133
+#133 pages
 
-pdf_urls = []
+link_list = []
 
-for i in (range(1,2)):
-    url = "https://www.pnas.org/content/by/section/Psychological%20and%20Cognitive%20Sciences?page=" + str(i)
+for 1 in (range(1,2)):
+    url = "https://www.pnas.org/content/by/section/Psychological%20and%20Cognitive%20Sciences?page=" + str(1)
     req = requests.get(url)
     soup = BeautifulSoup(req.text, 'lxml')
 
     for link in soup.find_all(class_='highwire-cite-metadata-pages'):
-        nihan = str(link)
-        pdf_urls.append(nihan[nihan.find('">')+2:nihan.find(";")])
+        elem_to_str = str(link)
+        link_list.append(elem_to_str[elem_to_str.find('">')+2:elem_to_str.find(";")])
 
-    print(*pdf_urls, sep='\n')
-    #yorumekledim
+print(len(link_list))
 
-    #print(ext_ids)
+pdf_urls = []
+elem = []
 
-    #print(find_ids)
-    #pdf_urls.append(soup.find_all(class_='highwire-cite-metadata-pages'))
-    #print(pdf_urls)
+for i in range(len(link_list)):
+    elem = ["https://www.pnas.org/content/pnas/" + str(link_list[i][-3:]) + "/" + str(link_list[i][1:3]) + "/" + str(link_list[i]) + ".full.pdf"]
+    pdf_urls.append(elem)
+    i += 1
+
+print(*pdf_urls, sep='\n')
